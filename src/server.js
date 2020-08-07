97cc5dacba9ad796e33210c24545e951ebccffe3
Andre Venditti <am7venditti@gmail.com>
2020-08-07 16:11:37 -0300

    some changes in js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const proffys = [
         time_to: [1200]
     },
     {
-        name: "Chunulão Tococão", 
+        name: "ivel Maldade", 
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
         whatsapp: "(15)9856-6489", 
         bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
@@ -40,7 +40,7 @@ const express = require('express')
 const server = express()
 
 
-//configurar nunjucks
+//configure nunjucks
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
     express: server,
