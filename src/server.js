201e1794883614e318cdbf09b0264819dd5042be
Andre Venditti <am7venditti@gmail.com>
2020-08-09 20:14:40 -0300

    finshed js files and installed sql

--- src/server.js
+++ src/server.js
@@ -2,38 +2,91 @@
 //datas of "proffys" stored in beck-end
 const proffys = [
     {
-        name: "Diego Fernandes", 
+        name: "Jean Izidoro", 
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp: "(15)9856-6489", 
+        whatsapp: "15997556688", 
         bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
         subject: "Química", 
-        cost: "20", 
+        cost: "20,00", 
         weekday: [0], 
         time_from: [720], 
         time_to: [1200]
     },
     {
-        name: "ivel Maldade", 
+        name: "Ivel Maldade", 
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp: "(15)9856-6489", 
-        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject: "Química", 
-        cost: "20", 
+        whatsapp: "1598566489", 
+        bio: "Gosto de vender minha arte com as coisas que a natureza dá.  Biologia é o conhecimento do planeta no qual convivemos com os outros seres em comunhão. Cola que é tudo", 
+        subject: "Biologia", 
+        cost: "20,00", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1200]
+    },
+    {
+        name: "Jamelão da Silva", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "1565487556", 
+        bio: "Gosto de vender minha arte com as coisas que a natureza dá.  Biologia é o conhecimento do planeta no qual convivemos com os outros seres em comunhão. Cola que é tudo", 
+        subject: "Biologia", 
+        cost: "20,00", 
         weekday: [1], 
         time_from: [720], 
         time_to: [1200]
     }
 ]
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+//Functions
+function getSubjects (subjectNumber){
+    const arrayPosition = +subjectNumber - 1
+    return subjects[arrayPosition]
+}
 
 //all the lines below it's the get() for showing all archives html in servidor
 function pageLanding(req,res){
     return res.render("index.html") 
 }
 function pageStudy(req,res){
-    return res.render("study.html", {proffys})
+    const filters = req.query
+    return res.render("study.html", {proffys, filters, subjects, weekdays})
 }
 function pageGiveClasses(req,res){
-    return res.render("give-classes.html")
+    const datas = req.query
+    
+    //adicionando a lista de proffys
+    const isNotEmpty = Object.keys(datas).length != 0
+    //se tiver dados : add
+    if (isNotEmpty) {
+        datas.subject = getSubjects(datas.subject)
+
+        proffys.push(datas)
+
+        return res.redirect("/study")
+    }
+    //se não: not add
+   
+    return res.render("give-classes.html", {subjects, weekdays})
 }
 
 const express = require('express')
